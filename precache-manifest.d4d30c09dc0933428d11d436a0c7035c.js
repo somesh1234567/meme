8ba91f3eba6fd6c817a26fa35e9ed62b8b7b5ed2
somesh1234567 <sonupanigrahi28@gmail.com>
2020-07-22 22:14:36 +0530

    Updates

---
+++ precache-manifest.d4d30c09dc0933428d11d436a0c7035c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f07fadc8d3d740e74771ab68f945b6",
+    "url": "/meme/index.html"
+  },
+  {
+    "revision": "f881a8433f245a54627c",
+    "url": "/meme/static/css/main.a48bf660.chunk.css"
+  },
+  {
+    "revision": "6b1682d8a58b4dd2c5d9",
+    "url": "/meme/static/js/2.065a6228.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme/static/js/2.065a6228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f881a8433f245a54627c",
+    "url": "/meme/static/js/main.fe77e1a1.chunk.js"
+  },
+  {
+    "revision": "60ea7f8d63a6be53f64b",
+    "url": "/meme/static/js/runtime-main.15acd509.js"
+  }
+]);
